2864caddf12f30185292f4f7f0b4d64471e0c34a
GuinDev <guineveresiqaf@gmail.com>
2025-11-21 21:01:37 -0300

    fooiii, implementado banco de dados de pagamentos

--- Python_Gym/pages/gerencia_Pagamento.py
+++ Python_Gym/pages/gerencia_Pagamento.py
@@ -6,23 +6,8 @@ st.set_page_config(
 
 st.write("# Gerenciar Pagamentos")
 
-df=pd.DataFrame({
-    'ID': [1, 2,],
-    'Nome': ['joao silva','maria oliveira'],
-    'valor': [150.00, 200.00],
-    'data': ['2024-01-15', '2024-01-20'],
-    'Estado': ['pago', 'pendente'],
-    })
 
-st.dataframe(df.set_index('ID'))
+# Your query
+conn = st.connection('gym_db', type='sql')
+st.dataframe(conn.query('SELECT * FROM pagamentos').set_index('id'))
 
-conn = st.connection("postgresql", type="sql")
-
-# Perform query.
-df = conn.query('SELECT * FROM pagamentos;', ttl="10m")
-
-# Print results.
-for row in df.itertuples():
-    st.write(
-        f"ID: {row.ID}, Nome: {row.Nome}, Valor: {row.valor}, Data: {row.data}, Estado: {row.Estado}"
-    )
